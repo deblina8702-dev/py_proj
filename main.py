f70cb3463bf910cebd443f6d7d24cfbdd1470e00
Deblina <sdeblina87@gmail.com>
2025-03-22 21:15:18 +0530

    this is a sample proj

---
+++ main.py
@@ -0,0 +1,11 @@
+def is_prime(number):
+    if (number <= 0) or (number == 1):
+        return False
+    elif number > 1:
+        for i in range(2, number):
+            if number % i == 0:
+                return True
+            else:
+                return False
+    
+print(is_prime(9))  
